5315ecce8f67789f2c95debf63629b7af225385f
Dan Phiffer <dan@phiffer.org>
2018-10-23 10:38:22 -0400

    :mailbox: process/reply from incoming messages

--- organizer.network.js
+++ organizer.network.js
@@ -288,7 +288,7 @@ app.get('/join/:slug', async (req, rsp) => {
 				}
 			});
 		} else {
-			let invited_by = member.person_id;
+			let invited_by = invite.person_id;
 			await join_context(person, invite.context.id, invited_by);
 			rsp.redirect(`${config.base_url}/group/${invite.context.slug}`);
 		}
@@ -603,13 +603,84 @@ app.post('/api/send', async (req, rsp) => {
 
 });
 
-app.post('/api/reply', upload.none(), (req, rsp) => {
+app.post('/api/reply', upload.none(), async (req, rsp) => {
 
-	console.log(req.body);
+	try {
 
-	rsp.send({
-		'ok': true
-	});
+		let reply = req.body;
+
+		let id = reply.headers.match(/Message-Id: <([^>]+)>/i);
+		let in_reply_to = reply.headers.match(/In-Reply-To: <([^>]+)>/i);
+
+		let context_id = -1;
+		let message_id = -1;
+		let person_id = -1;
+
+		if (! id || ! reply_to) {
+			console.log('Could not parse reply.');
+			console.log(reply);
+			return;
+		}
+
+		id = id[1];
+		in_reply_to = in_reply_to[1];
+
+		let lines = reply.text.trim().split('\n');
+		let content = [];
+		let quoted = [];
+
+		for (let line of lines) {
+			if (line.match(/^>/)) {
+				quoted.push(line);
+			} else if (line.match(/^(>\s*)*---$/)) {
+				break;
+			} else {
+				if (quoted.length > 0) {
+					content = content.concat(quoted);
+					quoted = [];
+				}
+				content.push(line);
+			}
+		}
+
+		content = content.join('\n').trim();
+
+		let query = db.query(`
+			SELECT *
+			FROM email_tx
+			WHERE id = $1
+		`, [in_reply_to]);
+
+		if (query.rows.length > 0) {
+			let email_tx = query.rows[0];
+
+			context_id = parseInt(email_tx.context_id);
+			person_id = parseInt(email_tx.person_id);
+			in_reply_to = parseInt(email_tx.message_id);
+
+			let person = await get_person(person_id);
+			let message = await send_message(person, context_id, in_reply_to, content);
+			message_id = message.id;
+		}
+
+		let reply_json = JSON.stringify(reply);
+
+		await db.query(`
+			INSERT INTO email_rx
+			(id, message_id, person_id, reply_json, created)
+			VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP)
+		`, [id, message_id, person_id, reply_json]);
+
+		rsp.send({
+			'ok': true
+		});
+
+	} catch(err) {
+		console.log(err.stack);
+		rsp.status(500).send({
+			'ok': false
+		});
+	}
 
 });
 
@@ -993,9 +1064,9 @@ ${config.base_url}/leave/${member.leave_slug}`);
 				let email_id = rsp[0].headers['x-message-id'];
 				db.query(`
 					INSERT INTO email_tx
-					(id, message_id, person_id, created)
-					VALUES ($1, $2, $3, CURRENT_TIMESTAMP)
-				`, [email_id, message.id, member.person_id]);
+					(id, context_id, message_id, person_id, created)
+					VALUES ($1, $2, $3, $4, CURRENT_TIMESTAMP)
+				`, [email_id, message.context_id, message.id, member.person_id]);
 			}
 
 		}
@@ -1093,7 +1164,6 @@ function get_invite(slug) {
 				resolve(false);
 			} else {
 				invite = query.rows[0];
-				console.log(invite);
 				invite.person = await get_person(invite.person_id);
 				invite.context = await get_context(invite.context_id);
 				resolve(invite);
