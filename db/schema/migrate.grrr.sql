a37eba2a72d1b2d2846a4a1cd2472960e208691f
Dan Phiffer <dan@phiffer.org>
2018-11-05 09:39:35 -0500

    :bug: fix db migrate bug

--- db/schema/migrate.grrr.sql
+++ db/schema/migrate.grrr.sql
@@ -2,7 +2,7 @@ ALTER TABLE message_facet RENAME TO facet;
 ALTER TABLE facet RENAME COLUMN message_id TO target_id;
 ALTER TABLE facet RENAME COLUMN type TO facet_type;
 ALTER TABLE facet RENAME COLUMN item_num TO facet_num;
-ALTER TABLE facet ADD COLUMN target_type VARCHAR(255) NOT NULL;
+ALTER TABLE facet ADD COLUMN target_type VARCHAR(255);
 ALTER TABLE member ADD COLUMN id SERIAL PRIMARY KEY;
 
 DROP INDEX message_facet_idx;
