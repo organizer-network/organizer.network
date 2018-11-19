329c71bf420e80e0215932590e136fe0585f8231
Dan Phiffer <dan@phiffer.org>
2018-11-18 20:06:50 -0500

    :wrench: pg.pool and error/async fixes

--- test/server.js
+++ test/server.js
@@ -1,17 +1,29 @@
-var request = require('supertest');
-describe('loading express', () => {
+const request = require('supertest');
+const create_server = require('../lib/server');
+const db = require('../lib/db');
+
+describe('lib/server.js', () => {
+
 	var server;
+
 	beforeEach(() => {
-		server = require('../lib/server')();
+		server = create_server();
 	});
+
 	afterEach(() => {
 		server.close();
 	});
+
+	after(() => {
+		db.end();
+	});
+
 	it('responds to /', (done) => {
 		request(server)
 		.get('/')
 		.expect(200, done);
 	});
+
 	it('404 everything else', (done) => {
 		request(server)
 		.get('/foo/bar')
