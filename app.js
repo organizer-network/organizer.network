1c4a2c9e9b6398d5504fa2e57996be88fbe25b6e
Dan Phiffer <dan@phiffer.org>
2018-11-18 21:30:45 -0500

    :dizzy: /settings → routes/settings.js

--- app.js
+++ app.js
@@ -54,55 +54,7 @@ app.use(require('./routes/group_create'));		// /group
 app.use(require('./routes/group_index'));		// /group/:slug
 app.use(require('./routes/group_thread'));		// /group/:slug/:id
 app.use(require('./routes/join'));				// /join/:slug
-
-app.get('/settings', async (req, rsp) => {
-
-	try {
-
-		let person = await db.curr_person(req);
-
-		if (! person) {
-			let then = '/settings';
-			return rsp.render('page', {
-				title: 'Login to continue',
-				view: 'login',
-				content: {
-					invite: null,
-					then: then
-				}
-			});
-		}
-
-		let contexts = await db.get_contexts(person);
-
-		for (let context of contexts.member_of) {
-			let member = await db.get_member(person, context.id);
-			await db.add_facets(member, 'member', 'email');
-			if (! member.facets) {
-				member.facets = {};
-			}
-			if (! member.facets.email) {
-				member.facets.email = 'send';
-			}
-			context.email = member.facets.email;
-		}
-
-		rsp.render('page', {
-			title: 'Settings',
-			view: 'settings',
-			content: {
-				context: null,
-				contexts: contexts,
-				person: person
-			}
-		});
-
-	} catch(err) {
-		console.log(err.stack);
-		return utils.error_page(rsp, '500');
-	}
-
-});
+app.use(require('./routes/settings_index'));	// /settings
 
 app.get('/settings/:slug', async (req, rsp) => {
 
