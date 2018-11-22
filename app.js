c5fd149718d9fa675acf8a680815ffe4bf6d4181
Dan Phiffer <dan@phiffer.org>
2018-11-22 11:47:51 -0500

    :dizzy: /:slug → routes/profile.js

--- app.js
+++ app.js
@@ -71,47 +71,7 @@ app.use(require('./routes/api/delete'));          // /api/delete
 app.use(require('./routes/api/leave'));           // /api/leave
 app.use(require('./routes/api/join'));            // /api/join
 app.use(require('./routes/api/settings'));        // /api/settings
-
-app.use(async (req, rsp) => {
-
-	try {
-
-		let curr_id = null;
-		let curr = await db.curr_person(req);
-		if (curr) {
-			curr_id = curr.id;
-		}
-
-		if (utils.url_slug_match(req.path.substr(1))) {
-			let person = await db.get_person(req.path.substr(1));
-			if (person) {
-				let then = req.query.then;
-				if (then && ! (/^\//)) {
-					then = null;
-				}
-				rsp.render('page', {
-					title: person.name || 'Profile',
-					view: 'profile',
-					content: {
-						person: person,
-						edit: (req.query.edit == '1'),
-						base_url: config.base_url,
-						curr_id: curr_id,
-						then: then
-					}
-				});
-				return;
-			}
-		}
-
-		rsp.status(404);
-		utils.error_page(rsp, '404');
-
-	} catch(err) {
-		console.log(err.stack);
-		return utils.error_page(rsp, '500');
-	}
-});
+app.use(require('./routes/profile.js'));          // /:slug
 
 async function send_notifications(sender, message, from) {
 
