0c770797effe94df6c90189e362be1fb5b9a2787
Dan Phiffer <dan@phiffer.org>
2018-12-11 09:43:25 -0500

    :zap: improve upgrade script

--- bin/upgrade.sh
+++ bin/upgrade.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
+PROJECT=`dirname $WHOAMI`
+
+cd "$PROJECT"
 pm2 restart ecosystem.config.js --env maintenance
-cd db
+
+cd "$PROJECT/db"
 make migrate
-cd ..
+
+if [ ! -f "$PROJECT/instance.yml" ] ; then
+	cp "$PROJECT/instance.yml.example" "$PROJECT/instance.yml"
+fi
+
+cd "$PROJECT"
 pm2 restart ecosystem.config.js --env production
