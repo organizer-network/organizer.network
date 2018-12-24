5a07ab496ccf898b388a2638d12880379e6a6b4c
Dan Phiffer <dan@phiffer.org>
2018-12-24 17:52:17 -0500

    :sparkles: CREATE TABLE route

--- db/schema/setup.sql
+++ db/schema/setup.sql
@@ -74,3 +74,13 @@ CREATE TABLE IF NOT EXISTS email_rx (
 	reply_json TEXT,
 	created TIMESTAMP
 );
+
+CREATE TABLE IF NOT EXISTS route (
+	slug VARCHAR(255),
+	target_id INTEGER NOT NULL,
+	target_type VARCHAR(255) NOT NULL,
+	type VARCHAR(255) DEFAULT 'active',
+	updated TIMESTAMP,
+	created TIMESTAMP
+);
+CREATE INDEX route_idx ON route (slug, target_id, target_type);
